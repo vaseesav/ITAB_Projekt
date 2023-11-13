d9aa4b7a7b9582be09126197ded879c5bd5bc932
halfdemon72 <leon_g_schulz@protonmail.com>
2023-11-13 14:31:01 +0100

    Auslagerungstest

---
+++ assets/php/mobileHeader.php
@@ -0,0 +1,14 @@
+<header class="mobile-header">
+    <div class="hamburger-menu">
+        <input id="menu__toggle" type="checkbox" />
+        <label class="menu__btn" for="menu__toggle">
+          <span></span>
+        </label>
+
+        <ul class="menu__box">
+          <li><a class="menu__item" href="#">Erkunden</a></li>
+          <li><a class="menu__item" href="#">Über Uns</a></li>
+          <li><a class="menu__item" href="login.php">Anmelden / Registrieren</a></li>
+        </ul>
+      </div>
+  </header>

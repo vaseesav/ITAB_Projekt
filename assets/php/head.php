4dfba3d132b61764d8d528b66831b2325bf68488
vaseesav <byvaste@gmail.com>
2023-11-22 00:20:44 +0100

    Added color scheme css file

--- assets/php/head.php
+++ assets/php/head.php
@@ -11,4 +11,9 @@ $loggedIn = isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true;
     <meta name='viewport' content='width=device-width, initial-scale=1'>
 
     <!--CSS Files-->
+    <link rel="stylesheet" href="assets/css/light-colors.css">
+    <link rel="stylesheet" href="assets/css/preloader-style.css">
+    <link rel="stylesheet" href="assets/css/navbar-style.css">
+    <link rel="stylesheet" href="assets/css/navbar-mob-style.css">
+    <link rel="stylesheet" href="assets/css/footer-style.css">
     <link rel="stylesheet" href="assets/css/index-style.css">

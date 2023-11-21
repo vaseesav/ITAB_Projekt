6fa4fffcb046a9389d2cd8f0858199a60a5ded45
Salim <salim.mohammad.ber@mail.com>
2023-11-21 11:46:16 +0100

    agb&impressum fertig, erkunden&cookies in Bearbeit

---
+++ assets/php/cookies.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/cookies1.css">
+    <script src="assets/js/cookies1.js"></script>
+    <title>Cookie Hinweis</title>
+</head>
+<body>
+
+<div id="cookie-popup">
+    <p>mieteinander verwendet Cookies, um Ihnen ein optimales Erlebnis zu bieten. Cookies sind kleine Textdateien, die auf Ihrem Gerät gespeichert werden und uns dabei helfen, die Leistung unserer Website zu verbessern. Sie ermöglichen es uns auch, Ihnen relevante Inhalte und Werbung bereitzustellen.</p>
+    <p>Indem Sie auf "Akzeptieren" klicken, stimmen Sie der Verwendung aller Cookies zu. Sie können jedoch die Cookie-Einstellungen besuchen, um eine kontrollierte Zustimmung zu erteilen.</p>
+    <p>Erfahren Sie mehr in unserer <a href="Link zur Datenschutzrichtlinie" target="_blank">Datenschutzrichtlinie</a>.</p>
+    <button id="accept-button">Akzeptieren</button>
+    <button id="change-settings-button">Cookie-Einstellungen ändern</button>
+</div>
+
+</body>
+</html>
--- assets/php/head.php
+++ assets/php/head.php
@@ -12,3 +12,8 @@ $loggedIn = isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true;
 
     <!--CSS Files-->
     <link rel="stylesheet" href="assets/css/index-style.css">
+
+    <link rel="stylesheet" href="assets/css/cookies1.css">    
+    <script src="assets/js/cookies1.js" defer></script>
+    <?php include('cookies.php'); ?>
+

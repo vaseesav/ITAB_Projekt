4d1218ae272ffea5465e24da22a946938d7b3308
vaseesav <byvaste@gmail.com>
2023-11-23 17:54:53 +0100

    Suchergebnisse und Suchfilter auf einer Seite. Wechsel auf ajax

--- assets/js/suchmaschine.js
+++ assets/js/suchmaschine.js
@@ -9,44 +9,27 @@ document.addEventListener("DOMContentLoaded", function () {
             method: 'POST',
             body: formData
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            let ergebnisseDiv = document.querySelector('.result-container');
-            ergebnisseDiv.innerHTML = '';
+            .then(response => response.json())
+            .then(data => {
+                let ergebnisseDiv = document.querySelector('.result-container');
+                ergebnisseDiv.innerHTML = '';
 
-            if (data.length > 0) {
-                data.forEach(row => {
-                    let resultDiv = document.createElement('div');
-                    resultDiv.className = 'result';
+                if (data.length > 0) {
+                    data.forEach(row => {
+                        let resultDiv = document.createElement('div');
+                        resultDiv.className = 'result';
 
-                    resultDiv.innerHTML = '<h3>' + row.AnzeigenName + '</h3>' +
-                        '<p>Veranstaltungstyp: ' + row.Veranstaltungstyp + '</p>' +
-                        '<p>Beschreibung: ' + row.Beschreibung + '</p>' +
-                        '<p>Preis: ' + row.preis + '</p>';
+                        resultDiv.innerHTML = `<h3>${row.AnzeigenName}</h3>
+                        <p>Veranstaltungstyp: ${row.Veranstaltungstyp}</p>
+                        <p>Beschreibung: ${row.Beschreibung}</p>
+                        <p>Preis: ${row.preis}</p>`;
 
-                    ergebnisseDiv.appendChild(resultDiv);
-                });
-            } else {
-                ergebnisseDiv.innerHTML = '<p>Keine Ergebnisse gefunden.</p>';
-            }
-        })
-        .catch(error => console.error('Fehler:', error));
-    });
-});
-
-// Warte auf das Laden des DOMs
-document.addEventListener("DOMContentLoaded", function () {
-    // Hole das Range-Element und das Output-Element
-    let preisRange = document.getElementById("preisRange");
-    let preisOutput = document.getElementById("preisOutput");
-
-    // Aktualisiere das Output-Element, wenn sich der Range-Wert ändert
-    preisRange.addEventListener("input", function () {
-        preisOutput.value = preisRange.value;
+                        ergebnisseDiv.appendChild(resultDiv);
+                    });
+                } else {
+                    ergebnisseDiv.innerHTML = '<p>Keine Ergebnisse gefunden.</p>';
+                }
+            })
+            .catch(error => console.error('Fehler:', error));
     });
 });
--- assets/php-backend/suchmaschine-page/datenabfrage.php
+++ assets/php-backend/suchmaschine-page/datenabfrage.php
@@ -1,12 +1,11 @@
 <?php
 global $conn;
-
-// Datenbankverbindung (Kenndaten)
+// Verbindung zur Datenbank
 require '../../database/connect.php';
 
-// Hier wird die Suchlogik implementiert
+// Überprüfen, ob die Anfrage eine POST-Anfrage ist
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validierung der Benutzereingaben
+    // Sicherstellen, dass Eingaben korrekt behandelt werden
     $searchText = isset($_POST["searchText"]) ? htmlspecialchars($_POST["searchText"]) : "";
     $bundesland = isset($_POST["bundesland"]) ? htmlspecialchars($_POST["bundesland"]) : "";
     $stadt = isset($_POST["stadt"]) ? htmlspecialchars($_POST["stadt"]) : "";
@@ -21,58 +20,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $veranstaltungsdauer = isset($_POST["veranstaltungsdauer"]) ? htmlspecialchars($_POST["veranstaltungsdauer"]) : "";
     $postleitzahl = isset($_POST["postleitzahl"]) ? htmlspecialchars($_POST["postleitzahl"]) : "";
 
-    // Überprüfe, ob $sortieren ein gültiger Spaltenname ist
-    $allowedColumns = ["preis", "anzahlGaeste", "startdatum"]; // Füge hier alle erlaubten Spaltennamen hinzu
-    if (in_array($sortieren, $allowedColumns)) {
+    // Überprüfen, ob die Sortierung und Sortierreihenfolge gültig sind
+    $allowedColumns = ["preis", "anzahlGaeste", "startdatum"];
+    if (in_array($sortieren, $allowedColumns) && ($sortierreihenfolge == "aufsteigend" || $sortierreihenfolge == "absteigend")) {
+        // SQL-Abfrage vorbereiten
+        $stmt = $conn->prepare("SELECT * FROM anzeige AS a
+                                JOIN buchungszeitraum AS b ON a.AnzeigenID = b.AnzeigenID
+                                WHERE 
+                                (a.Veranstaltungstyp LIKE ? OR a.Beschreibung LIKE ?) AND 
+                                (a.Bundesland = ? OR ? = '') AND 
+                                (a.Stadt LIKE ? OR ? = '') AND 
+                                (a.Veranstaltungstyp LIKE ? OR ? = '') AND
+                                (a.anzahlGaeste >= ? OR ? = '') AND
+                                (a.preis BETWEEN ? AND ?) AND
+                                (a.Plz = ? OR ? = '') AND
+                                (b.Startdatum >= ? OR ? = '') AND
+                                (b.Enddatum <= ? OR ? = '')
+                                ORDER BY a.$sortieren " . ($sortierreihenfolge == "absteigend" ? "DESC" : "ASC"));
 
-        // Überprüfe, ob $sortierreihenfolge gültig ist (nur "aufsteigend" oder "absteigend" erlaubt)
-        if ($sortierreihenfolge == "aufsteigend" || $sortierreihenfolge == "absteigend") {
+        $stmt->bind_param("ssssssssssssssssss", $searchText, $searchText, $bundesland, $bundesland, $stadt, $stadt, $veranstaltungstyp, $veranstaltungstyp, $anzahlGaeste, $anzahlGaeste, $minPreis, $maxPreis, $postleitzahl, $postleitzahl, $startdatum, $startdatum, $enddatum, $enddatum);
 
-            // Einfache SQL-Abfrage mit vorbereitetem Statement
-            $stmt = $conn->prepare("SELECT * FROM anzeige AS a
-            JOIN buchungszeitraum AS b ON a.AnzeigenID = b.AnzeigenID
-            WHERE 
-            (a.Veranstaltungstyp LIKE ? OR a.Beschreibung LIKE ?) AND 
-            (a.Bundesland = ? OR ? = '') AND 
-            (a.Stadt LIKE ? OR ? = '') AND 
-            (a.Veranstaltungstyp LIKE ? OR ? = '') AND
-            (a.anzahlGaeste >= ? OR ? = '') AND
-            (a.preis BETWEEN ? AND ?) AND
-            (a.Plz = ? OR ? = '') AND
-            (b.Startdatum >= ? OR ? = '') AND
-            (b.Enddatum <= ? OR ? = '')
-            ORDER BY a.$sortieren " . ($sortierreihenfolge == "absteigend" ? "DESC" : "ASC"));
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-            $stmt->bind_param("ssssssssssssssssss", $searchText, $searchText, $bundesland, $bundesland, $stadt, $stadt, $veranstaltungstyp, $veranstaltungstyp, $anzahlGaeste, $anzahlGaeste, $minPreis, $maxPreis, $postleitzahl, $postleitzahl, $startdatum, $startdatum, $enddatum, $enddatum);
-
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            if ($result->num_rows > 0) {
-                // Hier werden die Suchergebnisse angezeigt
-                echo "<h2>Suchergebnisse:</h2>";
-                while ($row = $result->fetch_assoc()) {
-                    echo "<div class='result'>";
-                    echo "<h3>" . $row["AnzeigenName"] . "</h3>";
-                    echo "<p>Veranstaltungstyp: " . $row["Veranstaltungstyp"] . "</p>";
-                    echo "<p>Beschreibung: " . $row["Beschreibung"] . "</p>";
-                    echo "<p>Preis: " . $row["preis"] . "</p>";
-                    // Weitere Informationen können hier hinzugefügt werden
-                    echo "</div>";
-                }
-            } else {
-                echo "<p>Keine Ergebnisse gefunden.</p>";
+        $resultArray = [];
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                array_push($resultArray, $row);
             }
-
-            $stmt->close();
-        } else {
-            // Unerlaubter Wert für $sortierreihenfolge
-            echo "Ungültiger Wert für die Sortierreihenfolge.";
         }
+
+        // Ergebnisse als JSON zurückgeben
+        header('Content-Type: application/json');
+        echo json_encode($resultArray);
+
+        $stmt->close();
     } else {
-        // Unerlaubter Wert für $sortieren
-        echo "Ungültiger Wert für die Sortierung.";
+        // Ungültige Sortierung oder Sortierreihenfolge
+        header('Content-Type: application/json');
+        echo json_encode(["error" => "Ungültige Sortierparameter"]);
     }
+} else {
+    // Keine POST-Anfrage
+    header('Content-Type: application/json');
+    echo json_encode(["error" => "Ungültige Anfrage"]);
 }
 
 $conn->close();
